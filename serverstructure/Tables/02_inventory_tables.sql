c729278816c7717b8915a59b8019af2fbb6feb8c
enigmaSpy <soczekv18@gmail.com>
2026-02-21 22:41:44 +0100

    Add core tables for user management, inventory tracking, and logging. Downgrade to 11g version

--- serverstructure/Tables/02_inventory_tables.sql
+++ serverstructure/Tables/02_inventory_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Products(
-    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id NUMBER PRIMARY KEY,
     name VARCHAR2(255) NOT NULL,
     price_buy NUMBER(10, 2) NOT NULL,
     price_sell NUMBER(10, 2) NOT NULL,
@@ -7,11 +7,27 @@ CREATE TABLE Products(
     is_active NUMBER(1) DEFAULT 1 CHECK(is_active IN (0,1)) NOT NULL,
     id_supplier NUMBER REFERENCES Suppliers(id)
 );
+CREATE SEQUENCE seq_products START WITH 1 INCREMENT BY 1;
+CREATE OR REPLACE TRIGGER trg_products_id 
+BEFORE INSERT ON Products
+FOR EACH ROW 
+BEGIN
+    :NEW.id :=seq_products.NEXTVAL;
+END;
+/
 
 CREATE TABLE Inventory(
-    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id NUMBER PRIMARY KEY,
     quantity NUMBER DEFAULT 0 NOT NULL,
     id_product NUMBER REFERENCES Products(id) ON DELETE CASCADE,
     id_warehouse NUMBER REFERENCES Warehouses(id) ON DELETE CASCADE,
     CONSTRAINT uq_prod_ware UNIQUE (id_product, id_warehouse)
 );
+CREATE SEQUENCE seq_inventory START WITH 1 INCREMENT BY 1;
+CREATE OR REPLACE TRIGGER trg_inventory_id 
+BEFORE INSERT ON Inventory
+FOR EACH ROW 
+BEGIN
+    :NEW.id :=seq_inventory.NEXTVAL;
+END;
+/
--- serverstructure/Tables/03_log_tables.sql
+++ serverstructure/Tables/03_log_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Inventory_Logs(
-    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    type VARCHAR(10) CHECK (type IN ('ADD', 'REMOVE', 'CORRECTION')) NOT NULL,
+    id NUMBER PRIMARY KEY,
+    type VARCHAR2(10) CHECK (type IN ('ADD', 'REMOVE', 'CORRECTION')) NOT NULL,
     quantity NUMBER NOT NULL CHECK (quantity > 0),
     quantity_after NUMBER NOT NULL,
     historical_price_buy NUMBER(10,2),
@@ -9,10 +9,18 @@ CREATE TABLE Inventory_Logs(
     id_warehouse NUMBER REFERENCES Warehouses(id),
     id_product NUMBER REFERENCES Products(id),
     id_user NUMBER REFERENCES Users(id)
-)
+);
+CREATE SEQUENCE seq_inventory_logs START WITH 1 INCREMENT BY 1;
+CREATE OR REPLACE TRIGGER tgr_inventory_log_id 
+BEFORE INSERT ON Inventory_Logs
+FOR EACH ROW
+BEGIN
+    :NEW.id:=seq_inventory_logs.NEXTVAL;
+END;
+/ 
 
 CREATE TABLE Product_Logs(
-    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id NUMBER PRIMARY KEY,
     field_name VARCHAR2(12) CHECK (field_name IN ('price_buy', 'price_sell', 'description', 'name', 'is_active', 'id_supplier')) NOT NULL,
     old_value VARCHAR2(4000),
     new_value VARCHAR2(4000),
@@ -20,13 +28,29 @@ CREATE TABLE Product_Logs(
     occurred_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     id_product NUMBER REFERENCES Products(id),
     id_user NUMBER REFERENCES Users(id)
-)
+);
+CREATE SEQUENCE seq_product_logs START WITH 1 INCREMENT BY 1;
+CREATE OR REPLACE TRIGGER tgr_product_log_id 
+BEFORE INSERT ON Product_Logs
+FOR EACH ROW
+BEGIN
+    :NEW.id:=seq_product_logs.NEXTVAL;
+END;
+/ 
 
 CREATE TABLE Login_Logs (
-    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id NUMBER PRIMARY KEY,
     is_success NUMBER(1) CHECK (is_success IN (0,1)) NOT NULL,
     email_attempted VARCHAR2(255) NOT NULL,
     occurred_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     ipaddress VARCHAR2(45),
     id_user NUMBER REFERENCES Users(id) ON DELETE SET NULL
 );
+CREATE SEQUENCE seq_login_logs START WITH 1 INCREMENT BY 1;
+CREATE OR REPLACE TRIGGER trg_login_logs_id
+BEFORE INSERT ON Login_Logs
+FOR EACH ROW
+BEGIN
+    :NEW.id:=seq_login_logs.NEXTVAL;
+END;
+/
