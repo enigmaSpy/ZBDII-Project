7fa1d9ec1900c151c0ea11ad5814056a67acd8af
enigmaSpy <soczekv18@gmail.com>
2026-03-08 02:00:04 +0100

    fix: correct SQL view definition by moving ORDER BY clause to the correct position

--- serverstructure/views/product_desc.sql
+++ serverstructure/views/product_desc.sql
@@ -2,4 +2,4 @@ CREATE OR REPLACE VIEW v_product_desc AS
  SELECT p.id, p.name, p.price_buy, p.price_sell, p.description,
                p.is_active, p.id_supplier, s.name AS supplier_name
         FROM Products p
-        LEFT JOIN Suppliers s ON p.id_supplier = s.id
+        LEFT JOIN Suppliers s ON p.id_supplier = s.id ORDER BY p.id;
